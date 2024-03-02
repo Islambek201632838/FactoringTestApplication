db2b7ca656d8a717efa5fb58b4d17eedde0aaf6d
Isevich_savage <124886919+Islambek201632838@users.noreply.github.com>
2024-03-03 04:08:46 +0600

    Update OrderService.java

--- backend/src/main/java/com/factoring/task/service/OrderService.java
+++ backend/src/main/java/com/factoring/task/service/OrderService.java
@@ -24,25 +24,9 @@ public class OrderService {
         return modelMapper.map(savedOrder, OrderDTO.class);
     }
 
-    public OrderDTO getOrderById(int id) {
-        return orderRepository.findById(id)
-                .map(order -> modelMapper.map(order, OrderDTO.class))
-                .orElse(null);
-    }
-
-    public OrderDTO updateOrder(OrderDTO orderDTO) {
-        Order order = modelMapper.map(orderDTO, Order.class);
-        Order updatedOrder = orderRepository.save(order);
-        return modelMapper.map(updatedOrder, OrderDTO.class);
-    }
-
     public List<OrderDTO> getAllOrders() {
         return orderRepository.findAll().stream()
                 .map(order -> modelMapper.map(order, OrderDTO.class))
                 .collect(Collectors.toList());
-    }
-
-    public void deleteOrder(int id) {
-        orderRepository.deleteById(id);
     }
 }
